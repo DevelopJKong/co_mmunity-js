2ed7e0655a707e510b750536535c7045ff6093eb
DevelopJKong <ybaram40@gmail.com>
2021-07-07 17:57:48 +0900

    day3 시작

---
+++ day3/index.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("form");
+const inputFirst = form.querySelector("#input-first");
+const inputSecond = form.querySelector("#input-second");
+const inputThird = form.querySelector("#input-third");
+const mainBox = document.querySelector(".main-box");
+const ul = mainBox.querySelector("ul");
+const li = ul.querySelectorAll("li");
+
+
+
+
+function handlerFirstClickEvent(e){
+    e.preventDefault();
+    li[0].innerText = "모처럼이니";
+}
+
+function handlerSecondClickEvent(e){
+    e.preventDefault();
+    li[1].innerText = "각잡고 열심히 하면";
+}
+
+function handlerThirdClickEvent(e){
+    e.preventDefault();
+    li[2].innerText = "코린이 탈출이다";
+}
+
+
+
+
+
+inputFirst.addEventListener("click",handlerFirstClickEvent);
+inputSecond.addEventListener("click",handlerSecondClickEvent);
+inputThird.addEventListener("click",handlerThirdClickEvent);
