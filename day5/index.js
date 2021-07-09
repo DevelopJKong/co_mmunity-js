fcc52a71d5a249adaf8970a4a0c29394e7e8234a
DevelopJKong <ybaram40@gmail.com>
2021-07-09 23:30:09 +0900

    day5 완성 / fullPage.js 안쓰고 구현해보기

--- day5/index.js
+++ day5/index.js
@@ -1,3 +1,5 @@
+
+
 let numberVar = 1;
 let objectVar = {
     number:1
@@ -14,3 +16,31 @@ console.log(typeof undefinedVar);
 console.log(typeof objectVar);
 console.log(typeof booleanVar);
 console.log(typeof nullVar);
+
+const body = document.querySelector("body");
+const div = body.querySelectorAll("div");
+const divHeight = div[0].scrollHeight;
+
+window.addEventListener("scroll",function(){
+
+    if( window.scrollY<=divHeight){
+        body.querySelector(".content-first").style.opacity = 1;
+
+    }else if(window.scrollY>divHeight && window.scrollY<=divHeight*2){
+
+        body.querySelector(".content-second").style.opacity = 1;
+
+    }else if(window.scrollY>divHeight*2 && window.scrollY<=divHeight*3){
+
+        body.querySelector(".content-third").style.opacity = 1;
+
+    }else if(window.scrollY>divHeight*3 && window.scrollY<=divHeight*4){
+
+        body.querySelector(".content-fourth").style.opacity = 1;
+        
+    } else {
+
+        body.querySelector(".content-fifth").style.opacity = 1;
+    } 
+});
+
