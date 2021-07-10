39827938a678a597ae77921dd955cd62ee43c1f2
DevelopJKong <ybaram40@gmail.com>
2021-07-10 14:06:46 +0900

    day5 scroll-nap / wheel 이벤트 구현

--- day5/index.js
+++ day5/index.js
@@ -18,29 +18,50 @@ console.log(typeof booleanVar);
 console.log(typeof nullVar);
 
 const body = document.querySelector("body");
-const div = body.querySelectorAll("div");
-const divHeight = div[0].scrollHeight;
+const div = body.querySelector("div");
+const section = div.querySelectorAll("section");
+const sectionHeight = section[0].scrollHeight;
+let count = 0;
 
-window.addEventListener("scroll",function(){
-
-    if( window.scrollY<=divHeight){
-        body.querySelector(".content-first").style.opacity = 1;
-
-    }else if(window.scrollY>divHeight && window.scrollY<=divHeight*2){
-
-        body.querySelector(".content-second").style.opacity = 1;
-
-    }else if(window.scrollY>divHeight*2 && window.scrollY<=divHeight*3){
-
-        body.querySelector(".content-third").style.opacity = 1;
-
-    }else if(window.scrollY>divHeight*3 && window.scrollY<=divHeight*4){
-
-        body.querySelector(".content-fourth").style.opacity = 1;
-        
-    } else {
-
-        body.querySelector(".content-fifth").style.opacity = 1;
-    } 
+div.addEventListener("wheel",function(){
+    if(count === 0){
+        div.querySelector(".content-first").style.opacity = 1;
+        count++;
+    }else if(count === 1){
+        div.querySelector(".content-second").style.opacity = 1;
+        count++;
+    }else if(count === 2) {
+        div.querySelector(".content-third").style.opacity = 1;
+        count++;
+    }else if(count === 3){
+        div.querySelector(".content-fourth").style.opacity = 1;
+        count++;
+    } else if(count === 4){
+        div.querySelector(".content-fifth").style.opacity = 1;
+        count++;
+    }
 });
+
+
+//     if( window.scrollY<=sectionHeight){
+//         
+//         div.querySelector(".content-first").style.opacity = 1;
+// 
+//     }else if(window.scrollY>sectionHeight && window.scrollY<=sectionHeight*2){
+// 
+//         div.querySelector(".content-second").style.opacity = 1;
+// 
+//     }else if(window.scrollY>sectionHeight*2 && window.scrollY<=sectionHeight*3){
+// 
+//         div.querySelector(".content-third").style.opacity = 1;
+// 
+//     }else if(window.scrollY>sectionHeight*3 && window.scrollY<=sectionHeight*4){
+// 
+//         div.querySelector(".content-fourth").style.opacity = 1;
+//         
+//     } else {
+// 
+//         div.querySelector(".content-fifth").style.opacity = 1;
+//     } 
+//})
 
