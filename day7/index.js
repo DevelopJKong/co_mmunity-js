27d07ede4fb40486cfcb2585ea6123bff17778c5
DevelopJKong <ybaram40@gmail.com>
2021-07-14 00:01:07 +0900

    day7 완성 css 복습 하기

--- day7/index.js
+++ day7/index.js
@@ -2,6 +2,10 @@ const div = document.querySelector(".main");
 const text = div.querySelector("h2");
 const restart = document.querySelector("#restart");
 const stop = document.querySelector("#stop");
+const mainP = div.querySelector("p");
+let stopCount = 0;
+let startCount = 0;
+let arr = [];
 
 function handlerTime(){
     const date = new Date();
@@ -19,9 +23,31 @@ setTimeout(()=>clearTimeout(repeat),3000);
 restart.addEventListener("click",function(e){
     e.preventDefault();
     repeat = setInterval(handlerTime,1000);
+    startCount++;
+    if(startCount <10){
+        mainP.innerText = `${startCount}번 클릭하셨네요!`;
+        arr.push(startCount);
+        for(let i = 0;  i < arr.length; i++){
+            console.log(arr[i]);
+        }
+    } else if( startCount > 10){
+        mainP.innerText = `멈춰!`;
+        startCount = 0;
+        arr.length = 0;
+    
+    }
+
 });
 stop.addEventListener("click",function(e){
     e.preventDefault();
     setTimeout(()=>clearTimeout(repeat),0);
+    stopCount++;
+    if(stopCount <10){
+        mainP.innerText = `${stopCount}번 클릭하셨네요!`;
+    } else if( stopCount > 10){
+        mainP.innerText = `멈춰!`;
+        stopCount = 0;
+    }
+
 });
 
