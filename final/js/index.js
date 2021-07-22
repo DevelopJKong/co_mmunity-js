1dff593c98077d70086c79e50766a83dec5300bb
DevelopJKong <ybaram40@gmail.com>
2021-07-22 15:57:59 +0900

    final chatBox 수정

---
+++ final/js/index.js
@@ -0,0 +1,15 @@
+const chat = document.querySelector(".chatbot");
+const chatContent = document.querySelector(".chatbot-content");
+const text = chatContent.querySelector(".chatbot-content__text");
+const closeIcon = text.querySelector("i");
+let count = 0;
+
+chat.addEventListener("click",function(){
+        chatContent.style.display = "flex";
+        chat.style.display = "none";
+});
+
+closeIcon.addEventListener("click",function(){
+    chatContent.style.display = "none";
+    chat.style.display = "flex";
+});
